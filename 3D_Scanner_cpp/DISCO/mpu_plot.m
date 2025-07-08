8b3a93d1509426edad1b2f1fb357e172001bd8f1
BarbraGitonga <gitongabarbra@gmail.com>
2025-07-09 00:55:02 +0300

    Refactor mpu_plot.m to update data source and improve plot clarity
    - Changed data source from "mpu_data.csv" to "LoggedData.csv".
    - Updated variable names for clarity: 'ax' to 'roll' and adjusted corresponding plots.
    - Removed unnecessary subplot for Y-axis data, focusing on roll and pitch orientation.
    - Adjusted plot legends and titles for better understanding of displayed data.

--- 3D_Scanner_cpp/DISCO/mpu_plot.m
+++ 3D_Scanner_cpp/DISCO/mpu_plot.m
@@ -1,25 +1,16 @@
-data = csvread("mpu_data.csv");     % Load CSV data
-ax = data(:, 1);
+data = csvread("LoggedData.csv");     % Load CSV data
+roll = data(:, 1);
 gx = data(:, 2);
-roll = data(:, 3);
-ay =  data(:, 4);
-gy =  data(:, 5);
-pitch =  data(:, 6);
-samples = (1:length(ax));  % X-axis
+pitch = data(:, 3);
+
+samples = (1:length(roll));  % X-axis
 
 figure;
-subplot(2,1,1);
-plot(samples, ax, 'r', samples, gx, 'g', samples, roll, 'b');
-legend("ax", "gx", "roll");
+plot(samples, roll, 'r', samples, gx, 'g', samples, pitch, 'b');
+legend("roll", "gx", "pitch");
 xlabel("Sample");
-ylabel("Angle (degrees)");
-title("Roll Orientation");
+ylabel("rad/s");
+title("Rolla nd pitch Orientation");
 grid on;
 
-subplot(2,1,2);
-plot(samples, ay, 'r', samples, gy, 'g', samples, pitch, 'b');
-legend('ay', 'gy', 'pitch');
-xlabel('Sample');
-ylabel('Value');
-title('Pitch Axis (Y) Data');
-grid on;
+
