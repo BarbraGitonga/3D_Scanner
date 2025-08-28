59f6e6c704a652737bb09b467169849900de5213
BarbraGitonga <gitongabarbra@gmail.com>
2025-08-28 13:04:20 +0300

    tof?

---
+++ 3D_Scanner_cpp/DISCO/3D-scanner/ToF/ToF.h
@@ -0,0 +1,16 @@
+#ifndef VL53L0X_H_
+#define VL53L0X_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f7xx_hal.h"
+
+#define VL53L0X_ADDR			0x29
+
+#define READ					0x52
+#define WRITE					0x53
+
+
+#endif // VL53L0X_H_
