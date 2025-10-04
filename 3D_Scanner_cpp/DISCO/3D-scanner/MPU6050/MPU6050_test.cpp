886ce5e8ac6a3ebe49ccb872994834677a2482d3
BarbraGitonga <gitongabarbra@gmail.com>
2025-10-04 11:39:58 +0300

    refactor: cleaned up repository and better arrangement of folders

--- 3D_Scanner_cpp/DISCO/3D-scanner/MPU6050/MPU6050_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * MPU6050_test.cpp
- *
- *  Created on: Jul 31, 2025
- *      Author: lenovo
- */
-
-#include <MPU6050/MPU6050.h>
-
--- 3D_Scanner_cpp/DISCO/3D-scanner/README.md
+++
@@ -1,3 +0,0 @@
-# Setup STM32Cube
-
-On cloning the repo, generate code from the .ioc file if using STM32F746G-DISCO board.
